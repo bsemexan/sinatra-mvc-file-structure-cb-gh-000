226f2a1ecd9a954af3d2721b74537c892bb65348
Berry  Semexan <bsemexan@gmail.com>
2018-03-04 04:07:02 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,8 +2,10 @@ class Dog
   attr_accessor :name, :breed, :age
 
   def initialize(name, breed, age)
-    @name = name 
-    @bredd = breed 
-    @age = age 
+    @name = name
+    @bredd = breed
+    @age = age
+  end
+  def self.all
   end
 end
